24d604a35d14b108e3e6e8aaea1e25ef284c2b9c
mattglbrt <matt@everyway.io>
2023-09-06 07:38:02 -0400

    First Commit

---
+++ src/data/info.ts
@@ -0,0 +1,145 @@
+export const info = {
+  baseUrl: "https://themattgilbert.com",
+  name: "Matt Gilbert",
+  jobDescription: "Growth Marketing & Demand Generation Leader",
+  about: `I have 18 years of experience in the marketing and information technology industries. 
+  I have led product marketing and demand generation teams for startups, entrepreneurs, and 
+  Fortune 100 companies for the last decade. I have helped startups go to market and become 
+  acquired, small businesses go from 0 to their first 10 million, and managed global marketing 
+  teams for publicly traded companies making 200 million a year. Additionally, I have extensive experience in
+  the government sector working with both government agencies fulfilling contracts, and working with
+  contractors to acquire them.
+  `,
+
+  experience: [
+    {
+      name: "Everyway",
+      location: "Owner",
+      startDate: "January 2022",
+      endDate: "Current",
+      description: [
+        "- Developing screens and UI components for the web application using React and Tailwind.",
+        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+      ],
+    },
+
+    {
+      name: "Invisible Commerce",
+      location: "Head of Product Marketing",
+      startDate: "July 2022",
+      endDate: "June 2023",
+      description: [
+        "Executed a comprehensive rebranding and go-to-market strategy encompassing various platforms including websites, apps, and sales enablement resources.",
+        "Designed and implemented robust outbound and inbound marketing systems, incorporating paid advertising, outbound email sequences, and content marketing.",
+        "- Acheived 1200% increase in LinkedIn engagement.",
+        "- Reached Page 1 in Google SERP within 30 days of content strategy going live",
+        "- Increased website traffic by 3000 visitors per month",
+      ],
+    },
+
+    {
+      name: "Edgio (Previously Limelight Networks)",
+      location: "Community Growth Consultant",
+      startDate: "July 2022",
+      endDate: "June 2023",
+      description: [
+        "I served as a consultant for the JavaScript Jam podcast and the Composability Summit 2022. My responsibilities included the development and implementation of an outbound email campaign, which successfully secured 27 leading industry speakers. I also handled all aspects of video production, design, and marketing related to the summit. The event achieved over 130,000 views and attracted 5,000 registered attendees.",
+    
+      ],
+    },
+    {
+      name: "Limelight Networks",
+      location: "Director of Global Demand Generation",
+      startDate: "September 2021",
+      endDate: "January 2022",
+      description: [
+        "Lead global marketing teams located in the United States, UK, India, and Japan while managing $30,000 monthly ad spend. Directly reported to the VP of Demand Gen to develop and coordinate all inbound and outbound marketing campaigns.",
+    
+      ],
+    },
+    {
+      name: "Layer0",
+      location: "Head of Marketing",
+      startDate: "March 2021",
+      endDate: "September 2021",
+      description: [
+        "Conducted a full rebrand and go to market campaign with the Director of Growth to lead the teams that resulted in a $50 million acquisition (Limelight Networks) within 7 months of the rebrand.",
+    
+      ],
+    },
+    {
+      name: "IntellecTechs",
+      location: "Director of Growth",
+      startDate: "July 2015",
+      endDate: "March 2021",
+      description: [
+        " I oversaw all website development, graphic design, social media, and client marketing campaigns. During this time I was responsible for obtaining commercial and government net new sales of $500,000 per year .",
+    
+      ],
+    },
+    {
+      name: "Marketing Consultant",
+      location: "Multiple Companies",
+      startDate: "April 2014",
+      endDate: "July 2015",
+      description: [
+        "- Bang Muay Thai San Jose: Acheived membership goal within 3 months and gained enough revenue to construct an entirely new gym.",
+        "- Tactical Distributors: Worked as part of a 2 man team that increased gross daily sales from $4,000 to $12,000.",
+        "- Bluewater Sales: achieved a full rebrand of the company, built a full online sales pipleline via an ecommerce store and government sector website while conducting GSA,DOD EMALL, HUBZONE and direct sales face to face with the Army, Navy and Air Force.",
+        "- Everest Tactical: Increased the commercial side of the company from ($8000) per month to a net revenueof $35,000 per month within 6 months.",
+    
+      ],
+    },
+    {
+      name: "ipConfigure",
+      location: "Marketing Manager",
+      startDate: "Septermber 2013",
+      endDate: "April 2014",
+      description: [
+        " accomplished a company-wide rebrand that consisted of 2 new websites, a new logo design, full corporate identity and documentation rebranding. I worked indepth with the sales team to create tools and resources to facilitate product sales, including an internal sales hub. I also planned, scheduled and provided printed materials for trade shows and training courses.",
+    
+      ],
+    },
+    {
+      name: "IntellecTechs",
+      location: "Marketing Manager",
+      startDate: "Sep 2011",
+      endDate: "April 2013",
+      description: [
+        "I maitained over 120 Social Media Profiles, built over 30 websites and created hundreds of print and graphic design collateral. I also supported the US Navy, and US Airforce via software development projects.",
+    
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Old Dominion University",
+      location: "Norfolk, Virginia",
+      startDate: "2008",
+      endDate: "2012",
+      description: [
+        "Bachelor of Applied Science, Pscyhology",
+      ],
+    },
+  
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/mattglbrt",
+    twitter: "https://twitter.com/mattglbrt",
+    github: "https://github.com/mattglbrt",
+    email: "matt@goeveryway.com",
+    linkedin: "https://www.linkedin.com/in/mattglbrt/",
+  },
+
+  projects: [
+    {
+      title: "Astro Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+  ],
+};
